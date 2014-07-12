aa9b5cc658dcdb6d805ffe9eeb5b25c6a0399bee
Adam Dehnel (home) <arsdehnel@gmail.com>
2014-07-11 21:40:36 -0500

    Added HTML page that renders JSON results in table

--- polite-crawler-0.3.0--aslaninst.com/crawler.js
+++ polite-crawler-0.3.0--aslaninst.com/crawler.js
@@ -1,19 +1,32 @@
+// sample command
+// casperjs crawler.js --startUrl=http://aslaninst.com/ --datafile=data.json --resultfile=results.json --screenshots
+
+
 // Create instances
 var casper = require('casper').create({ /*verbose: true, logLevel: 'debug'*/ });
 var utils = require('utils')
 var fs = require('fs')
 var functions = require('functions')
+var results = [];
 
-// Set the start URL
 if( casper.cli.has('startUrl') ){
 	var startUrl = casper.cli.raw.get('startUrl');
 }
 
 var baseUrl = startUrl.substring(0,startUrl.length-1);
 
-// get the datafile name
 if( casper.cli.has('datafile') ){
 	var datafile = casper.cli.raw.get('datafile');
+}
+
+if( casper.cli.has('resultfile') ){
+	var resultfile = casper.cli.raw.get('resultfile');
+}
+
+if( casper.cli.has('screenshots') ){
+	var screenshots = true;
+}else{
+	var screenshots = false;
 }
 
 // import it
@@ -37,7 +50,7 @@ function spider(url) {
 	if( functions.indexOf.call(visited, url) === -1 ){
 		visited.push(url);
 	}else{
-		console.log(casper.colorizer.format('REPEAT', { fg: 'red', bold: true})+' already visited '+url);
+		casper.log(casper.colorizer.format('REPEAT', { fg: 'red', bold: true})+' already visited '+url, 'error');
 		//return;
 	}
 
@@ -48,16 +61,17 @@ function spider(url) {
 	switch( urlStatus ){
 
 		case "allowed":
+		case "check":
 
 			casper.open(url).then(function() {
 
-				//this.echo(url.replace( baseUrl, '').replace(/\//g,'_').substring(1,2000));
-				var captureFilename = url.replace( baseUrl, '').replace(/\//g,'_').replace(/\?/g,'').substring(1,2000) + '.png';
-				if( captureFilename === '.png' ){
-					captureFilename = 'home.png';
+				if( screenshots ){
+					var captureFilename = url.replace( baseUrl, '').replace(/\//g,'_').replace(/\?/g,'').substring(1,2000) + '.png';
+					if( captureFilename === '.png' ){
+						captureFilename = 'home.png';
+					}
+					this.capture('screenshots/'+captureFilename);
 				}
-				//this.echo( captureFilename );
-				this.capture('screenshots/'+captureFilename);
 
 				// Set the status style based on server status code
 				var status = this.status().currentHTTPStatus;
@@ -68,28 +82,33 @@ function spider(url) {
 
 				// Display the spidered URL and status
 				this.echo(this.colorizer.format(status, statusStyle) + ' ' + url);
+				results.push({url:url,status:urlStatus.toUpperCase() + ' (' + status + ')' });
 
-				// Find links present on this page
-				var links = this.evaluate(function() {
-					var links = [];
-					Array.prototype.forEach.call(__utils__.findAll('a'), function(e) {
-						links.push(e.getAttribute('href'));
+				if( status === 200 && urlStatus === "allowed" ){
+
+					// Find links present on this page
+					var links = this.evaluate(function() {
+						var links = [];
+						Array.prototype.forEach.call(__utils__.findAll('a'), function(e) {
+							links.push(e.getAttribute('href'));
+						});
+						return links;
 					});
-					return links;
-				});
 
-				// Add newly found URLs to the stack
-				Array.prototype.forEach.call(links, function(link) {
+					// Add newly found URLs to the stack
+					Array.prototype.forEach.call(links, function(link) {
 
-					if( link.substring(0,1) === '/' ){
-						link = baseUrl + link;
-					}
+						if( link.substring(0,1) === '/' ){
+							link = baseUrl + link;
+						}
 
-					if( functions.indexOf.call( visited, link ) === -1 && functions.indexOf.call( pendingUrls, link ) === -1 ) {
-						//casper.echo( 'visited check: link '+link+', result '+indexOf.call( visited, link )+', visitedLength'+visited.length)
-						pendingUrls.push(link);
-					}
-				});
+						if( functions.indexOf.call( visited, link ) === -1 && functions.indexOf.call( pendingUrls, link ) === -1 ) {
+							pendingUrls.push(link);
+						}
+					});
+
+
+				}
 
 				// If there are URLs to be processed
 				if ( pendingUrls.length > 0) {
@@ -102,24 +121,21 @@ function spider(url) {
 			break;
 
 		case "blocked":
-			casper.echo(casper.colorizer.format('BLOCKED',{ fg: 'red', bold: true }) + ' ' + url);
-			if ( pendingUrls.length > 0) {
-				var nextUrl = pendingUrls.shift();
-				spider(nextUrl);
-			}
-			break;
-
 		case "pending":
-			casper.echo(casper.colorizer.format('PENDING',{ fg: 'blue', bold: true }) + ' ' + url);
-			if ( pendingUrls.length > 0) {
-				var nextUrl = pendingUrls.shift();
-				spider(nextUrl);
-			}
-			break;
-
 		default:
-			json.urls.push({"url":url.replace( baseUrl, ''),"status":"pending"});
-			casper.echo(casper.colorizer.format('ADDED',{ fg: 'yellow', bold: true }) + ' ' + url);
+
+			if( urlStatus === "blocked" ){
+				casper.echo(casper.colorizer.format('BLOCKED',{ fg: 'red', bold: true }) + ' ' + url);
+				results.push({url:url,status:urlStatus.toUpperCase()});
+			}else if( urlStatus === "pending" ){
+				casper.echo(casper.colorizer.format('PENDING',{ fg: 'blue', bold: true }) + ' ' + url);
+				results.push({url:url,status:urlStatus.toUpperCase()});
+			}else{
+				json.urls.push({"url":url.replace( baseUrl, ''),"status":"pending"});
+				casper.echo(casper.colorizer.format('ADDED',{ fg: 'yellow', bold: true }) + ' ' + url);
+				results.push({url:url,status:'ADDED'});
+			}
+
 			if ( pendingUrls.length > 0) {
 				var nextUrl = pendingUrls.shift();
 				spider(nextUrl);
@@ -137,6 +153,7 @@ casper.start('http://www.google.com/', function() {
 
 casper.then(function(){
 	fs.write(datafile, JSON.stringify(json,null,'\t'), 'w');
+	fs.write(resultfile, JSON.stringify(results,null,'\t'), 'w');
 })
 
 
